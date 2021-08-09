e35b7f8938915a84008d08b6643a5412ae48c622
Evan Kohout <evankohout@Evans-MacBook-Pro.local>
2021-08-09 01:36:11 -0400

    added sanity schemas, objects and plugs

---
+++ studio/schemas/documents/route.js
@@ -0,0 +1,102 @@
+import client from 'part:@sanity/base/client'
+import { MdLink } from 'react-icons/md'
+
+function myAsyncSlugifier(input) {
+  const query = '*[_id == $id][0]'
+  const params = {id: input._ref}
+  return client.fetch(query, params).then(doc => {
+    return doc.title.toLowerCase().replace(/\s+/g, '-').slice(0, 200)
+  })
+}
+
+export default {
+  name: 'route',
+  type: 'document',
+  title: 'Landing page routes',
+  icon: MdLink,
+  initialValue: {
+    useSiteTitle: false,
+  },
+  fieldsets: [
+    {
+      title: 'Visibility',
+      name: 'visibility',
+    },
+  ],
+  fields: [
+    {
+      name: 'page',
+      type: 'reference',
+      validation: (Rule) => Rule.required(),
+      description: 'The page you want to appear at this path. Remember it needs to be published.',
+      to: [
+        {
+          type: 'page',
+        },
+      ],
+    },
+    {
+      name: 'slug',
+      type: 'slug',
+      description: 'This is the website path the page will accessible on',
+      title: 'Path',
+      validation: (Rule) =>
+        Rule.required().custom((slug) => {
+          if (slug && slug.current && slug.current === '/') {
+            return 'Cannot be /'
+          }
+          return true
+        }),
+      options: {
+        source: 'page',
+        // Read more: https://www.sanity.io/docs/slug-type
+        slugify: myAsyncSlugifier
+      }
+    },
+    {
+      title: 'Use site title?',
+      description:
+        'Use the site settings title as page title instead of the title on the referenced page',
+      name: 'useSiteTitle',
+      type: 'boolean',
+    },
+    {
+      title: 'Open graph',
+      name: 'openGraph',
+      description: 'These values populate meta tags',
+      type: 'openGraph',
+    },
+    {
+      title: 'Include in sitemap',
+      description: 'For search engines. Will be generateed to /sitemap.xml',
+      name: 'includeInSitemap',
+      type: 'boolean',
+      fieldset: 'visibility'
+    },
+    {
+      title: 'Disallow in robots.txt',
+      description: 'Hide this route for search engines like google',
+      name: 'disallowRobots',
+      type: 'boolean',
+      fieldset: 'visibility'
+    },
+    {
+      name: 'campaign',
+      type: 'string',
+      title: 'Campaign',
+      description: 'UTM for campaings'
+    },
+  ],
+  preview: {
+    select: {
+      title: 'slug.current',
+      subtitle: 'page.title',
+    },
+    prepare({ title, subtitle }) {
+      return {
+        title: ['/', title].join(''),
+        subtitle,
+      }
+    },
+  },
+}
--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -1,19 +1,57 @@
-// First, we must import the schema creator
 import createSchema from 'part:@sanity/base/schema-creator'
 
-// Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 // document schemas
+import navMenu from './documents/navMenu'
 import author from './documents/author'
+import category from './documents/category'
+import post from './documents/post'
+import page from './documents/page'
+import siteSettings from './documents/siteSettings'
+import route from './documents/route'
 
-// Then we give our schema to the builder and provide the result to Sanity
+import simpleBlockContent from './objects/simpleBlockContent'
+
+import * as plugs from './plugs'
+import plugDefaultFields from './plugs/_plugDefaultFields'
+
+// Object types
+import { instagram, videoEmbed } from './objects/embeds'
+import cta from './objects/cta'
+import bodyPortableText from './objects/bodyPortableText'
+import excerptPortableText from './objects/excerptPortableText'
+import mainImage from './objects/mainImage'
+import authorReference from './objects/authorReference'
+import link from './objects/link'
+import variation from './objects/variation'
+import openGraph from './objects/openGraph'
+
+const allPlugs = Object.values(plugs).map((plug) => {
+  return { ...plug, fields: plugDefaultFields.concat(plug.fields) }
+})
+
 export default createSchema({
-  // We name our schema
   name: 'default',
-  // Then proceed to concatenate our document type
-  // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    author
-  ]),
+    variation,
+    openGraph,
+    route,
+    link,
+    simpleBlockContent,
+    cta,
+    siteSettings,
+    post,
+    navMenu,
+    page,
+    category,
+    author,
+    mainImage,
+    authorReference,
+    instagram,
+    videoEmbed,
+    bodyPortableText,
+    excerptPortableText,
+  ])
+  .concat(allPlugs),
 })
