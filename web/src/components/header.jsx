23855334bddd2c7624fbc3faaeb8231ef4c3edc0
Evan Kohout <evankohout@Evans-MacBook-Pro.local>
2021-08-14 20:25:04 -0400

    BMWCCAPSR-2 removed old tailwinds css on headerbar

--- web/src/components/header.jsx
+++ web/src/components/header.jsx
@@ -19,16 +19,7 @@ export function Logo() {
 
 const Header = ({ showNav, siteTitle, scrolled, navMenuItems = [] }) => {
 
-  let headerClass = "fixed w-full z-30 top-0 text-white";
-  headerClass += scrolled ? " bg-white shadow" : "";
 
-  let navActionClass =
-    "mx-auto lg:mx-0 hover:underline font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75";
-
-  let navContentClass =
-    "w-full flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block mt-2 lg:mt-0 text-black p-4 lg:p-0 z-20";
-
-  let titleClass = "toggleColour no-underline hover:no-underline font-bold text-2xl lg:text-4xl";
   return (
 
     <header
