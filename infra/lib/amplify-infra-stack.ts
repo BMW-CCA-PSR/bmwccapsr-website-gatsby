cc4ab469968d74075c2b8323172da74b6f96c267
Evan Kohout <evankohout@Evans-MacBook-Pro.local>
2021-08-19 02:44:25 -0400

    BMWCCAPSR-14 added infra changes for fargate cluster

--- infra/lib/amplify-infra-stack.ts
+++ infra/lib/amplify-infra-stack.ts
@@ -1,10 +1,19 @@
 import * as cdk from '@aws-cdk/core';
 import * as amplify from "@aws-cdk/aws-amplify";
 import { App } from '@aws-cdk/core';
+import path = require('path');
+import {core,
+  aws_ecs, 
+  aws_ecs_patterns,
+  aws_iam,
+  aws_ec2
+ } from aws_cdk 
 
 export class AmplifyInfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // setup amplify app to github
 
     const amplifyApp = new amplify.App(this, "bmwccapsr-website", {
       sourceCodeProvider: new amplify.GitHubSourceCodeProvider({
@@ -15,6 +24,8 @@ export class AmplifyInfraStack extends cdk.Stack {
         }),
       }),
     });
+
+    // branch setup
 
     const masterBranch = amplifyApp.addBranch("master");
     const devBranch = amplifyApp.addBranch("dev");
@@ -47,5 +58,19 @@ export class AmplifyInfraStack extends cdk.Stack {
     // dev env vars
     devBranch.addEnvironment("STAGE", "dev");
     devBranch.addEnvironment("GATSBY_SANITY_DATASET", "beta");
+
+      // Preview server docker 
+      // dev
+
+      const role = aws_iam.Role(this, "FargateContainerRole", aws_iam.ServicePrincipal("ecs-tasks.amazonaws.com"))
+      const vpc = new aws_ec2.Vpc(this, "PreviewServerVPC", { maxAzs: 2 });
+      const cluster = new aws_ecs.Cluster(this, 'Cluster', { vpc }); 
+
+      new aws_ecs_patterns.ApplicationLoadBalancedFargateService(this, "FargateService", {
+        cluster,
+        taskImageOptions: {
+          image: aws_ecs.ContainerImage.fromAsset(path.resolve(__dirname, 'local-image'))
+        },
+      });
   }
 }
