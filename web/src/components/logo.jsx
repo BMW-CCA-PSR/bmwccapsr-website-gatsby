04436c1599ed4a8c8806ff7f554e955e93af3760
Evan Kohout <evankohout@Evans-MacBook-Pro.local>
2021-10-04 11:34:49 -0400

    adjusted new logo size for mobile

--- web/src/components/logo.jsx
+++ web/src/components/logo.jsx
@@ -17,7 +17,7 @@ function Logo() {
           layout="constrained"
           sx={{
             objectFit: "cover",
-            width: ["141px", "141px", "175px", "175px"],
+            width: ["130px", "130px", "175px", "175px"],
             boxShadow: "0 2px 4px -1px rgba(0, 0, 0, 0.2), 0 1px 10px 0 rgba(0, 0, 0, 0.12), 0 4px 5px 0 rgba(0, 0, 0, 0.14)",
           }}
         />
