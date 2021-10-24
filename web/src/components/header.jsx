a63aa434395ecd11ee46612332b7f4571db999c4
Evan Kohout <evankohout@Evans-MacBook-Pro.local>
2021-10-24 02:05:14 -0400

    fixed mobile menu

--- web/src/components/header.jsx
+++ web/src/components/header.jsx
@@ -1,24 +1,30 @@
 /** @jsxImportSource theme-ui */
 import { useBreakpointIndex } from '@theme-ui/match-media';
-import { Heading, Container, Flex, Divider, Button, Box, MenuButton, Close } from 'theme-ui';
+import { Container, Flex, Divider, MenuButton, Close, Text } from 'theme-ui';
 import { Link } from 'gatsby';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Dropdown from './dropdown';
 import NavLink from './navLink';
 import Logo from './logo';
-import MobileMenu from './mobile-menu';
+import MobileNavLink from './mobileNavLink';
+import MobileCTALink from './mobileCTALink';
 
 const Header = ({ showNav, siteTitle, scrolled, navMenuItems = [] }) => {
 	const [ isToggledOn, setToggle ] = useState(false);
 	const toggle = () => setToggle(!isToggledOn);
 	const index = useBreakpointIndex();
 
+	const html = document.querySelector('html')
+	useEffect(() => {
+		isToggledOn ? (html.style.overflow = 'hidden') : (html.style.overflow = 'visible')
+	  }, [isToggledOn])
+
 	return (
 		<nav
 			sx={{
 				backgroundColor: 'lightgray',
 				position: 'relative',
-				zIndex: '2',
+				zIndex: 10,
         borderBottom: "4px solid",
         borderBottomColor: "primary",
         boxShadow: '0 3px 5px -1px rgba(0, 0, 0, 0.3), 0 1px 18px 0 rgba(0, 0, 0, 0.32), 0 6px 10px 0 rgba(0, 0, 0, 0.24)'
@@ -82,19 +88,89 @@ const Header = ({ showNav, siteTitle, scrolled, navMenuItems = [] }) => {
 											sx={{ display: 'block', py: '20px' }}
 											onClick={toggle}
 										/>
-									) : (
-										<Close
-											aria-label="close menu"
-											sx={{ display: 'block', py: '20px', zIndex: "5"}}
-											onClick={toggle}
-										/>
-									)}
+									) : null }
 								</div>
 							)}
 						</div>
 					)}
 				</Flex>
-				<Flex>{isToggledOn ? <MobileMenu navItems={navMenuItems} /> : null}</Flex>
+				<Flex>
+					{isToggledOn ? 
+					<div sx={{
+						backgroundColor: 'lightgray',
+						position: 'fixed',
+						width: '100%',
+						height: "100vh",
+						display: 'block',
+						my: "-60px",
+						py: "40px",
+						z: 5
+						
+					}}>
+					<div sx={{ my: "-20px",textAlign: "right"}}>
+						<Close
+							aria-label="close menu"
+							onClick={toggle}
+						/>
+					</div>
+					<ul
+						sx={{
+							listStyle: 'none',
+							textAlign: 'center',
+							m: 0,
+							p: 0,
+							backgroundColor: 'lightgray',
+
+						}}
+					>
+						{navMenuItems.map((i) => {
+							const link = i.navigationItemUrl;
+							if (i.navigationItemUrl) {
+								return (
+									<ul
+										sx={{
+											listStyle: 'none',
+											margin: 0,
+											padding: 0,
+											color: 'text'
+										}}
+									>
+										<li
+											sx={{
+												display: 'block',
+												height: '100%'
+												//mx: "0.5rem",
+											}}
+										>
+											<Divider sx={{ color: 'darkgray' }} />
+											<Text variant="text.label">{i.title}</Text>
+											{link.items && link.items.length > 0 ? (
+												<div
+													sx={{
+														listStyle: 'none',
+														m: 0,
+														p: 0,
+														display: 'flex',
+														flexDirection: 'column',
+														cursor: 'pointer'
+													}}
+												>
+													{link.items.map((subLink) => <MobileNavLink {...subLink} />)}
+												</div>
+											) : null}
+										</li>
+									</ul>
+								);
+							} else if (i._type == 'link') {
+								return <MobileNavLink key={i._key} {...i} />;
+							} else if (i._type == 'cta') {
+								return <MobileCTALink key={i._key} {...i} />;
+							}
+						})}
+					</ul>
+					</div>
+					: null}
+				</Flex>
 			</Container>
 		</nav>
 	);
