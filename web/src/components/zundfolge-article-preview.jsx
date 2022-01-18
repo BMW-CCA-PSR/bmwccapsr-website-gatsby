a3363a414f3cbb65e1a8582477071bb2e0a5fef0
Evan Kohout <evankohout@Evans-MacBook-Pro.local>
2022-01-18 10:21:07 -0500

    fixed issue with props on zundfolge article preview

--- web/src/components/zundfolge-article-preview.jsx
+++ web/src/components/zundfolge-article-preview.jsx
@@ -1,57 +1,71 @@
 /** @jsxImportSource theme-ui */
-import { format, parseISO } from "date-fns";
-import { Link } from "gatsby";
-import React from "react";
-import { Card, Box, Text, Heading, Flex, Badge } from "theme-ui"
-import { getZundfolgeUrl } from "../lib/helpers";
-import SanityImage from "gatsby-plugin-sanity-image"
+import { format, parseISO } from 'date-fns';
+import { Link } from 'gatsby';
+import React from 'react';
+import { Card, Box, Text, Heading, Flex, Badge } from 'theme-ui';
+import { getZundfolgeUrl } from '../lib/helpers';
+import SanityImage from 'gatsby-plugin-sanity-image';
 
 function ZundfolgeArticlePreview(props) {
-  const bg = typeof(props.mainImage.asset.metadata.palette.dominant.background) !== 'undefined' ? props.mainImage.asset.metadata.palette.dominant.background : "lightgrey"
-  const fg = typeof(props.mainImage.asset.metadata.palette.dominant.foreground) !== 'undefined' ? props.mainImage.asset.metadata.palette.dominant.foreground : "black"
-  return (
-    <Link
-      to={getZundfolgeUrl(props.slug.current)}
-      sx={{textDecoration: "none"}}
-    >
-      <Card
-        sx={{
-          textDecoration: "none",
-          background: `linear-gradient(to top, transparent 0%, ${bg} 100%)`,
-          width: "100%",
-          height: "100%",
-          mx: "auto",
-          borderRadius: "8px",
-          position: "relative"
-        }}
-      >
-          {props.mainImage && props.mainImage.asset && (
-            <SanityImage {...props.mainImage} width={1440}
-              sx={{
-                position: "absolute", 
-                width: "100%", 
-                height: "100%", 
-                objectFit: "cover",
-                borderRadius: "8px",
-                zIndex: "-1",
-              }} 
-            />
-          )}
-        <Box p={3} sx={{}}>
-          <Heading sx={{ textDecoration: "none", variant: "styles.h3", color: `${fg}`,}} >{props.title}</Heading>
-          <Text sx={{
-            color: `${fg}`
-          }}>{format(parseISO(props.publishedAt), "MMMM do, yyyy")}
-          </Text>
-          <Flex>
-            {props.categories && props.categories.map(cat => (
-              <Badge mr={3}>{cat.title}</Badge>
-            ))}
-          </Flex>
-        </Box>
-      </Card>
-    </Link>
-  );
+	const bg =
+		typeof props.mainImage !== 'undefined'
+			? props.mainImage.asset.metadata.palette.dominant.background
+			: 'lightgrey';
+	const fg =
+		typeof props.mainImage !== 'undefined' 
+      ? props.mainImage.asset.metadata.palette.dominant.foreground 
+      : 'black';
+	return (
+		<div>
+			{props.mainImage &&
+			props.title && (
+				<Link to={getZundfolgeUrl(props.slug.current)} sx={{ textDecoration: 'none' }}>
+					<Card
+						sx={{
+							textDecoration: 'none',
+							background: `linear-gradient(to top, transparent 0%, ${bg} 100%)`,
+							width: '100%',
+							height: '100%',
+							mx: 'auto',
+							borderRadius: '8px',
+							position: 'relative'
+						}}
+					>
+						{props.mainImage &&
+						props.mainImage.asset && (
+							<SanityImage
+								{...props.mainImage}
+								width={1440}
+								sx={{
+									position: 'absolute',
+									width: '100%',
+									height: '100%',
+									objectFit: 'cover',
+									borderRadius: '8px',
+									zIndex: '-1'
+								}}
+							/>
+						)}
+						<Box p={3} sx={{}}>
+							<Heading sx={{ textDecoration: 'none', variant: 'styles.h3', color: `${fg}` }}>
+								{props.title}
+							</Heading>
+							<Text
+								sx={{
+									color: `${fg}`
+								}}
+							>
+								{format(parseISO(props.publishedAt), 'MMMM do, yyyy')}
+							</Text>
+							<Flex>
+								{props.categories && props.categories.map((cat) => <Badge mr={3}>{cat.title}</Badge>)}
+							</Flex>
+						</Box>
+					</Card>
+				</Link>
+			)}
+		</div>
+	);
 }
 
 export default ZundfolgeArticlePreview;
