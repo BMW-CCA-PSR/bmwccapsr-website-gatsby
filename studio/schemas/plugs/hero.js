2f0e7bdd3fad6833b90076318fb82f6314b32283
Evan Kohout <evankohout@Evans-MacBook-Pro.local>
2022-02-10 08:31:24 -0500

    added validation for heros

--- studio/schemas/plugs/hero.js
+++ studio/schemas/plugs/hero.js
@@ -11,12 +11,13 @@ export default {
         name: 'heading',
         type: 'string',
         title: 'Heading',
-        validation: Rule => Rule.max(60).warning(`A title shouldn't be more than 60 characters.`)
+        validation: Rule => Rule.max(60).error(`A title shouldn't be more than 60 characters.`)
       },
       {
         name: 'tagline',
         type: 'string',
-        title: 'Tagline'
+        title: 'Tagline',
+        validation: Rule => Rule.max(60).error(`A tagline shouldn't be more than 60 characters.`)
       },
       {
         name: 'image',
