0e08b97d89b3a65f5b8443cc47a0f2b8b6b448c8
Evan Kohout <ebox86@gmail.com>
2022-03-04 01:16:44 -0500

    fixed CTA button hover color for consistency

--- web/src/components/CTALink.jsx
+++ web/src/components/CTALink.jsx
@@ -20,7 +20,7 @@ var style = {
   boxShadow: "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
   "&:hover":{
     color: "white",
-    bg: "secondary",
+    bg: "highlight",
   }
 }
 
