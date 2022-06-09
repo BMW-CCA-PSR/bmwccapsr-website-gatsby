b1ff426cfe6126cc31c6c6aa8d1802036d09d530
Evan Kohout <ebox86@gmail.com>
2022-06-08 21:24:47 -0400

    bumped versions and added new gatsby document action but found bug so commented out for now

--- studio/src/documentActions/index.js
+++ studio/src/documentActions/index.js
@@ -1,11 +1,12 @@
 import defaultResolver from 'part:@sanity/base/document-actions'
+import { gatsbyPreviewAction } from "sanity-plugin-gatsby-cloud-preview";
 import {types as workflowTypes} from '../config/workflow'
 import {resolveWorkflowActions} from './workflow'
 
 export default function resolveDocumentActions(docInfo) {
   if (workflowTypes.includes(docInfo.type)) {
-    return resolveWorkflowActions(docInfo)
+    return [...resolveWorkflowActions(docInfo)] //, gatsbyPreviewAction]
   }
 
-  return defaultResolver(docInfo)
+  return [...defaultResolver(docInfo)] //, gatsbyPreviewAction]
 }
